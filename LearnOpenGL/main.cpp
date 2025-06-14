77c5fda6ad5d99b2acdcd6ae8af734ca89cb726c
Johnny Wannamaker <j.wannamaker19@gmail.com>
2025-06-14 13:08:11 -0700

    Added the most simple vertex shader ever by defining the GLSL code as a cstring as a const global, and in the render loop creating a shader object, attaching the cstring as the source code of the shader object, and compiling with glCompileShader(vertexShader). Also added a check for if the shader compiled correctly.

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -8,6 +8,13 @@ using namespace std;
 const unsigned int WINDOW_WIDTH = 800;
 const unsigned int WINDOW_HEIGHT = 600;
 
+const char* vertexShaderSource = "#version 330 core\n"
+	"layout (location = 0) in vec3 aPos;\n"
+	"void main()\n"
+	"{\n"
+	"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+	"}\0";
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
@@ -54,6 +61,20 @@ int main()
 		glGenBuffers(1, &VBO);
 		glBindBuffer(GL_ARRAY_BUFFER, VBO);
 		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+		unsigned int vertexShader;
+		vertexShader = glCreateShader(GL_VERTEX_SHADER);
+		glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+		glCompileShader(vertexShader);
+
+		int success;
+		char infoLog[512];
+		glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+		if (!success)
+		{
+			glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+			cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << endl;
+		}
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
