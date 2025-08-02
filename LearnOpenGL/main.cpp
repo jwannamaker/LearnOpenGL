44c8ab6634ea42f70884c6bb617e559d3742b83e
Johnny Wannamaker <j.wannamaker19@gmail.com>
2025-08-02 13:43:27 -0700

    One dark code blocks, embed youtube video, new animation

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -5,7 +5,6 @@
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/quaternion.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <array>
 #include <cstring>
@@ -22,6 +21,7 @@ const unsigned int WINDOW_HEIGHT = 1000;
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 unsigned int loadTexture(const char* path, int* imageWidth, int* imageHeight);
+float triangleWave(float time, float amplitude, float period, float verticalOffset, float phaseOffset);
 
 int main()
 {
@@ -56,26 +56,21 @@ int main()
 	// Build and compile shader program
 	Shader myShader("shader.vert", "shader.frag");
 
-	// Setup textures
-	int imageWidth, imageHeight;
-	unsigned int texture0 = loadTexture("sunnyswamp-32x.png", &imageWidth, &imageHeight);
-	//unsigned int texture1 = loadTexture("sunnyswamp-32x.png", &imageWidth, &imageHeight);
-
 	// Setup model, view, and projection matrices
 	mat4 model = mat4(1.0f), view = mat4(1.0f), projection = mat4(1.0f);
-	projection = ortho(-20.0f, 20.0f, -20.0f, 20.0f, -20.0f, 20.0f);
+	view = rotate(view, radians(-65.0f), vec3(1.0f, 0.0f, 0.0f));
+	view = translate(view, vec3(0.0f, 40.0f, -20.0f));
+	projection = ortho(-20.0f, 20.0f, -20.0f, 20.0f, -20.0f, 100.0f);
 
 	myShader.use();
-	glUniform1i(glGetUniformLocation(myShader.ID, "texture0"), 0);
-	glUniform1i(glGetUniformLocation(myShader.ID, "texture1"), 1);
 	glUniformMatrix4fv(glGetUniformLocation(myShader.ID, "model"), 1, GL_FALSE, value_ptr(model));
 	glUniformMatrix4fv(glGetUniformLocation(myShader.ID, "view"), 1, GL_FALSE, value_ptr(view));
 	glUniformMatrix4fv(glGetUniformLocation(myShader.ID, "projection"), 1, GL_FALSE, value_ptr(projection));
 
 	// Setup vertex data/buffers and configure vertex attributes
 	float x = 1.0f, y = 1.0f, z = 1.0f;
+	float dx = 3.0f, dy = 3.0f, dz = 3.0f;
 	vec3 center = vec3(0.0f, 0.0f, 0.0f);
-	vec3 delta = vec3(2.5f, 2.5f, 2.5f);
 	GLfloat vertices[] = {
 		// positions	// texture coords
 		-x, -y, -z,		0.0f, 0.0f,
@@ -121,35 +116,35 @@ int main()
 		-x,  y, -z,		0.0f, 1.0f
 	};
 	array<vec3, 27> positions = {
-		vec3(center.x - 5.0f, center.y - 5.0f, center.z - 5.0f),
-		vec3(center.x + 0.0f, center.y - 5.0f, center.z - 5.0f),
-		vec3(center.x + 5.0f, center.y - 5.0f, center.z - 5.0f),
-		vec3(center.x - 5.0f, center.y + 0.0f, center.z - 5.0f),
-		vec3(center.x + 0.0f, center.y + 0.0f, center.z - 5.0f),
-		vec3(center.x + 5.0f, center.y + 0.0f, center.z - 5.0f),
-		vec3(center.x - 5.0f, center.y + 5.0f, center.z - 5.0f),
-		vec3(center.x + 0.0f, center.y + 5.0f, center.z - 5.0f),
-		vec3(center.x + 5.0f, center.y + 5.0f, center.z - 5.0f),
+		vec3(center.x -   dx, center.y -   dy, center.z -   dz),
+		vec3(center.x + 0.0f, center.y -   dy, center.z -   dz),
+		vec3(center.x +   dx, center.y -   dy, center.z -   dz),
+		vec3(center.x -   dx, center.y + 0.0f, center.z -   dz),
+		vec3(center.x + 0.0f, center.y + 0.0f, center.z -   dz),
+		vec3(center.x +   dx, center.y + 0.0f, center.z -   dz),
+		vec3(center.x -   dx, center.y +   dy, center.z -   dz),
+		vec3(center.x + 0.0f, center.y +   dy, center.z -   dz),
+		vec3(center.x +   dx, center.y +   dy, center.z -   dz),
 
-		vec3(center.x - 5.0f, center.y - 5.0f, center.z + 0.0f),
-		vec3(center.x + 0.0f, center.y - 5.0f, center.z + 0.0f),
-		vec3(center.x + 5.0f, center.y - 5.0f, center.z + 0.0f),
-		vec3(center.x - 5.0f, center.y + 0.0f, center.z + 0.0f),
+		vec3(center.x -   dx, center.y -   dy, center.z + 0.0f),
+		vec3(center.x + 0.0f, center.y -   dy, center.z + 0.0f),
+		vec3(center.x +   dx, center.y -   dy, center.z + 0.0f),
+		vec3(center.x -   dx, center.y + 0.0f, center.z + 0.0f),
 		vec3(center.x + 0.0f, center.y + 0.0f, center.z + 0.0f),
-		vec3(center.x + 5.0f, center.y + 0.0f, center.z + 0.0f),
-		vec3(center.x - 5.0f, center.y + 5.0f, center.z + 0.0f),
-		vec3(center.x + 0.0f, center.y + 5.0f, center.z + 0.0f),
-		vec3(center.x + 5.0f, center.y + 5.0f, center.z + 0.0f),
+		vec3(center.x +   dx, center.y + 0.0f, center.z + 0.0f),
+		vec3(center.x -   dx, center.y +   dy, center.z + 0.0f),
+		vec3(center.x + 0.0f, center.y +   dy, center.z + 0.0f),
+		vec3(center.x +   dx, center.y +   dy, center.z + 0.0f),
 
-		vec3(center.x - 5.0f, center.y - 5.0f, center.z + 5.0f),
-		vec3(center.x + 0.0f, center.y - 5.0f, center.z + 5.0f),
-		vec3(center.x + 5.0f, center.y - 5.0f, center.z + 5.0f),
-		vec3(center.x - 5.0f, center.y + 0.0f, center.z + 5.0f),
-		vec3(center.x + 0.0f, center.y + 0.0f, center.z + 5.0f),
-		vec3(center.x + 5.0f, center.y + 0.0f, center.z + 5.0f),
-		vec3(center.x - 5.0f, center.y + 5.0f, center.z + 5.0f),
-		vec3(center.x + 0.0f, center.y + 5.0f, center.z + 5.0f),
-		vec3(center.x + 5.0f, center.y + 5.0f, center.z + 5.0f),
+		vec3(center.x -   dx, center.y -   dy, center.z +   dz),
+		vec3(center.x + 0.0f, center.y -   dy, center.z +   dz),
+		vec3(center.x +   dx, center.y -   dy, center.z +   dz),
+		vec3(center.x -   dx, center.y + 0.0f, center.z +   dz),
+		vec3(center.x + 0.0f, center.y + 0.0f, center.z +   dz),
+		vec3(center.x +   dx, center.y + 0.0f, center.z +   dz),
+		vec3(center.x -   dx, center.y +   dy, center.z +   dz),
+		vec3(center.x + 0.0f, center.y +   dy, center.z +   dz),
+		vec3(center.x +   dx, center.y +   dy, center.z +   dz),
 	};
 
 	GLuint VAO, VBO;
@@ -163,34 +158,50 @@ int main()
 	glEnableVertexAttribArray(1);
 	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
 
-	quat q = quat_cast(model);
-	quat p = quat_cast(mat4(1.0f));
+	unsigned int colorLoc = glGetUniformLocation(myShader.ID, "customColor");
+	vec4 customColor = vec4(0.0f);
+
+	float positionScale = 0.0f, positionCheck = 0.0f, t = 0.0f;
 	while (!glfwWindowShouldClose(window))
 	{
 		processInput(window);
 
-		glClearColor(0.4f, 0.45f, 0.502f, 1.0f);
+		glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		glBindVertexArray(VAO);
 		
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, texture0);
-
-		//glActiveTexture(GL_TEXTURE1);
-		//glBindTexture(GL_TEXTURE_2D, texture1);
-		
 		myShader.use();
-		float t = glfwGetTime();
+		t = glfwGetTime();
 		view = mat4(1.0f);
-		//view = translate(view, vec3(0.0f, 0.0f, -10.0f));
-		view = rotate(view, radians(t * 20.0f), vec3(1.0f, 1.0f, cos(t)));
+		view = rotate(view, 2.0f, vec3(cos(1.5f * t), 0.0f, sin(1.5f * t)));
 		for (unsigned int i = 0; i < positions.size(); i++)
 		{
 			model = mat4(1.0f);
-			model = translate(model, positions[i] * (1.5f + 0.5f * cos(4.5f * sin(0.8f * t))));
-			model = rotate(model, t * radians(50.0f), vec3(0.25 * sin(t), 0.25 * cos(t), 0.0f));
+			positionScale = 1.0f;
+			positionCheck = abs(positions[i].x) + abs(positions[i].y) + abs(positions[i].z);
+			customColor = vec4(1.0f);
+			// outer corners
+			if (positionCheck == abs(dx) + abs(dy) + abs(dz))
+			{
+				positionScale = triangleWave(t, 1.25f, 10.0f * half_pi<float>(), 2.25f, half_pi<float>() * 1.0f);
+			}
+			// outer edges
+			if (positionCheck == abs(dx) + abs(dy) || positionCheck == abs(dx) + abs(dz) || positionCheck == abs(dy) + abs(dz))
+			{
+				positionScale = triangleWave(t, 1.5f, 10.0f * half_pi<float>(), 2.0f, half_pi<float>() * 1.0f);
+			}
+			// outer faces
+			if (positionCheck == abs(dx) || positionCheck == abs(dy) || positionCheck == abs(dz))
+			{
+				positionScale = triangleWave(t, 2.0f, 10.0f * half_pi<float>(), 3.0f, half_pi<float>() * 1.0f);
+			}
+
+			vec3 newPosition = view * vec4(positions[i], 1.0f);
+			customColor = vec4(newPosition, positionScale * 0.1f);
+			model = translate(model, positions[i] * positionScale);
 			glUniformMatrix4fv(glGetUniformLocation(myShader.ID, "model"), 1, GL_FALSE, value_ptr(model));
+			glUniform4fv(colorLoc, 1, value_ptr(customColor));
 			glDrawArrays(GL_TRIANGLES, 0, 36);
 		}
 		glUniformMatrix4fv(glGetUniformLocation(myShader.ID, "view"), 1, GL_FALSE, value_ptr(view));
@@ -232,19 +243,25 @@ unsigned int loadTexture(const char* path, int* imageWidth, int* imageHeight)
 	if (!imageData)
 	{
 		cout << "Failed to load texture" << endl;
-		return -1;
 	}
 
 	if (strstr(path, ".png"))
 	{
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, *imageWidth, *imageHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, imageData);
 	}
-	else if (strstr(path, ".jpg") || strstr(path, ".jpeg"))
+	if (strstr(path, ".jpg") || strstr(path, ".jpeg"))
 	{
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, *imageWidth, *imageHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, imageData);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, *imageWidth, *imageHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, imageData);
 	}
 	glGenerateMipmap(GL_TEXTURE_2D);
 	stbi_image_free(imageData);
 
 	return textureID;
+}
+
+float triangleWave(float time, float amplitude, float period, float verticalOffset, float phaseOffset)
+{
+	float a = 2.0f * amplitude / pi<float>();
+	float k = 2.0f * pi<float>() / period;
+	return a * asin(sin(k * time + phaseOffset)) + verticalOffset;
 }
--- LearnOpenGL/shader.frag
+++ LearnOpenGL/shader.frag
@@ -4,10 +4,9 @@ out vec4 FragColor;
 
 in vec2 TexCoord;
 
-uniform sampler2D texture0;
-// uniform sampler2D texture1;
+uniform vec4 customColor;
 
 void main()
 {
-	FragColor = texture(texture0, TexCoord);
+	FragColor = customColor;
 }
