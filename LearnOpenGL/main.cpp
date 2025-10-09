606d607b638a0f085b337321389c2ce8d0054de0
Johnny Wannamaker <j.wannamaker19@gmail.com>
2025-10-09 08:36:22 -0700

    Camera notes 1, with a tiny bit of added flare

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -20,67 +20,6 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 unsigned int loadTexture(const char* path, int* imageWidth, int* imageHeight);
 
-struct Camera {
-	mat4 view;
-	vec3 position;
-
-	Camera() 
-	{ 
-		view = mat4(1.0f);
-		position = vec3(0.0f);
-	}
-	
-	void turn(vec2 amount)
-	{
-		view = rotate(view, amount.x * rotateSpeed(), up()); 
-		view = rotate(view, amount.y * rotateSpeed(), right());
-	}
-
-	vec3 front(void)
-	{
-		return vec3(view * vec4(0.0f, 0.0f, 1.0f, 1.0f));
-	}
-
-	vec3 right(void)
-	{
-		return vec3(view * vec4(1.0f, 0.0f, 0.0f, 1.0f));
-	}
-
-	vec3 up(void)
-	{
-		return cross(front(), right());
-	}
-
-	float rotateSpeed(void) { return radians(0.05f); }
-};
-
-void processInput(GLFWwindow* window, Camera& camera)
-{
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-	{
-		glfwSetWindowShouldClose(window, true);
-	}
-
-	if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
-	{
-		camera.turn(vec2(+1.0f, 0.0f));
-
-	}
-	if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
-	{
-		camera.turn(vec2(-1.0f, 0.0f));
-	}
-
-	if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
-	{
-		camera.turn(vec2(0.0f, -1.0f));
-	}
-	if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
-	{
-		camera.turn(vec2(0.0f, +1.0f));
-	}
-}
-
 int main()
 {
 	// Initialize and configure GLFW
@@ -120,12 +59,22 @@ int main()
 	GLuint texture0 = loadTexture("container.jpg", &imageWidth, &imageHeight);
 	glUniform1i(glGetUniformLocation(myShader.ID, "texture0"), 0);
 
-	// Setup model, view, and projection matrices
-	mat4 model = mat4(1.0f), projection = mat4(1.0f);
-	Camera camera = Camera();
-	projection = perspective(radians(45.0f), 1.0f, 0.1f, 100.0f);
+
+	// Setup camera and MVP matrices
+	float radius = 5.0f;
+	float cameraX = sin(glfwGetTime()) * radius;
+	float cameraY = sin(glfwGetTime());
+	float cameraZ = cos(glfwGetTime()) * radius;
+	
+	mat4 model = mat4(1.0f);
+
+	mat4 view = lookAt(vec3(cameraX, 0.0f, cameraZ),		// position
+					   vec3(0.0f, 0.0f, 0.0f),				// target
+					   vec3(0.0f, 1.0f, 0.0f));				// up
+	
+	mat4 projection = perspective(radians(45.0f), 1.0f, 0.1f, 100.0f);
 	glUniformMatrix4fv(glGetUniformLocation(myShader.ID, "model"), 1, GL_FALSE, value_ptr(model));
-	glUniformMatrix4fv(glGetUniformLocation(myShader.ID, "view"), 1, GL_FALSE, value_ptr(camera.view));
+	glUniformMatrix4fv(glGetUniformLocation(myShader.ID, "view"), 1, GL_FALSE, value_ptr(view));
 	glUniformMatrix4fv(glGetUniformLocation(myShader.ID, "projection"), 1, GL_FALSE, value_ptr(projection));
 
 	// Setup vertex data/buffers and configure vertex attributes
@@ -184,56 +133,28 @@ int main()
 	glEnableVertexAttribArray(1);
 	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
 
-	GLfloat lineVertices[] = {
-		-0.5f, -0.5f, -0.5f,	
-		 1.0f, -0.5f, -0.5f,	
-
-		-0.5f, -0.5f, -0.5f,	
-		-0.5f,  1.0f, -0.5f,	
-
-		-0.5f, -0.5f, -0.5f,	
-		-0.5f, -0.5f,  1.0f
-	};
-
-	GLuint lineVAO, lineVBO;
-	glGenVertexArrays(1, &lineVAO);
-	glGenBuffers(1, &lineVBO);
-	glBindVertexArray(lineVAO);
-	glBindBuffer(GL_ARRAY_BUFFER, lineVBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(lineVertices), lineVertices, GL_STATIC_DRAW);
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), 0);
-
 	const vec4 bgColor = normalize(vec4(33.0f, 34.0f, 54.0f, 255.0f));
-	const vec3 noColor = vec3(0.0f);
-	const vec3 red = vec3(1.0f, 0.0f, 0.0f);
-	const vec3 green = vec3(0.0f, 1.0f, 0.0f);
-	const vec3 blue = vec3(0.0f, 0.0f, 1.0f);
 	
 	myShader.use();
+	glBindVertexArray(VAO);
 	glActiveTexture(GL_TEXTURE0);
 	glBindTexture(GL_TEXTURE_2D, texture0);
-	glLineWidth(2.0f);
 	while (!glfwWindowShouldClose(window))
 	{ 
-		processInput(window, camera);
-		glUniformMatrix4fv(glGetUniformLocation(myShader.ID, "view"), 1, GL_FALSE, value_ptr(camera.view));
+		processInput(window);
+
+		cameraX = sin(glfwGetTime()) * radius;
+		cameraY = sin(glfwGetTime());
+		cameraZ = cos(glfwGetTime()) * radius;
+		view = lookAt(vec3(cameraX, cameraY, cameraZ),
+					  vec3(0.0f, 0.0f, 0.0f),
+					  vec3(0.0f, 1.0f, 0.0f));
+		glUniformMatrix4fv(glGetUniformLocation(myShader.ID, "view"), 1, GL_FALSE, value_ptr(view));
 
 		glClearColor(bgColor.r, bgColor.g, bgColor.b, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		glBindVertexArray(VAO);
-		glUniform1i(glGetUniformLocation(myShader.ID, "useTexture"), GL_TRUE);
 		glDrawArrays(GL_TRIANGLES, 0, 36);
-
-		glBindVertexArray(lineVAO);
-		glUniform1i(glGetUniformLocation(myShader.ID, "useTexture"), GL_FALSE);
-		glUniform3fv(glGetUniformLocation(myShader.ID, "aColor"), 1, value_ptr(red));
-		glDrawArrays(GL_LINES, 0, 2);
-		glUniform3fv(glGetUniformLocation(myShader.ID, "aColor"), 1, value_ptr(blue));
-		glDrawArrays(GL_LINES, 2, 2);
-		glUniform3fv(glGetUniformLocation(myShader.ID, "aColor"), 1, value_ptr(green));
-		glDrawArrays(GL_LINES, 4, 2);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
--- LearnOpenGL/shader.frag
+++ LearnOpenGL/shader.frag
@@ -5,17 +5,9 @@ out vec4 FragColor;
 in vec2 TexCoord;
 
 uniform sampler2D texture0;
-uniform vec3 aColor;
 uniform bool useTexture;
 
 void main()
 {
-	if (useTexture)
-	{
-		FragColor = texture(texture0, TexCoord);
-	}
-	else
-	{
-		FragColor = vec4(aColor, 1.0);
-	}
+	FragColor = texture(texture0, TexCoord);
 }
